ee58a3faf54b634288d9f80244c50738b3af2a2a
andersongbrl <andersonpwf17@gmail.com>
2024-05-07 09:00:52 -0300

    Add files via upload

---
+++ # Pedindo ao usuário para inserir seu nome (feito po elfo).py
@@ -0,0 +1,14 @@
+# Pedindo ao usuário para inserir seu nome
+nome = input("Digite o seu nome: ")
+
+# Saúda o usuário
+print("Olá,", nome, "! Bem-vindo ao mundo da programação em Python!")
+
+#pedindo a idade do usuario
+idade = input("Qual sua idade: ")
+
+#respondendo usuario
+print("Que bom pensei que voce era mais novo! ") 
+
+#Pergunta ao usuario o que era pra ele estar fazendo
+nome = input("Mas não era pra voce estar prestando atenção na aula?")
